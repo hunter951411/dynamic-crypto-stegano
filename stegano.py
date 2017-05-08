42fc5cab3ee2a9fccd9c099b062be8d0fcb82b06
Administrator <admin@example.com>
2017-05-08 18:05:18 +0700

    Update Stegano.py

--- stegano.py
+++ stegano.py
@@ -4,6 +4,9 @@ Author: Sa Phi saphi070@gmail.com
 Perform embed and retriev binary to/from image
 """
 import sys
+import argparse
+import crypto
+import math
 from common import log
 LOG = log.setup_log(__name__)
 
@@ -92,24 +95,77 @@ class Stegano(object):
                 next_index2 = 7
         return binary
 
-
-def main():
+def start_embed(plaintext, key, image_embed, image_retriev):
     print "Embed binary in picture"
     print "+++++++++++++++++++++++"
-    s = Stegano('before.png')
-    binary = [1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1]
-    print "Perform embed list binary {0} on image {1}".format(binary,
-                                                              "before.png")
+    s = Stegano(image_embed)
+    binary = []
+    c = crypto.Crypto(key=key,
+               text=plaintext)
+    C = c.encrypt()
+    # Change binary in C to bits in binary
+    [[[[binary.append(int(l)) for l in k]for k in j]for j in i]for i in C]
     b = s.embed(binary=binary)
     print "======================="
     print "Write result in image after.png"
-    with open('after.png', 'wb') as f:
+    with open(image_retriev, 'wb') as f:
         f.write(bytearray(b))
     print "***********************"
+
+
+def start_retriev(key, image_retriev):
     print "Perform retriev binary in image after.png"
-    s = Stegano('after.png')
+    s = Stegano(image_retriev)
     b = s.retriev()
-    print "Result: ", b
+    C = []
+    LIST_BINARY = []
+    # Change bits to binary
+    for i in range(0, len(b), 8):
+        exp = 7   # exp: 7 -> 1
+        sum = 0   # sum is decimal of 8 bit sequent 
+        for j in b[0+i:8+i]:
+            sum = j * math.pow(2,exp) + sum
+            exp = exp-1
+        binary = format(int(sum), '08b')
+        LIST_BINARY.append(binary)
+    for i in range(0, len(LIST_BINARY), 4):
+        LIST_SPLIT = []
+        [LIST_SPLIT.append(j) for j in LIST_BINARY[0+i:4+i]]
+        C.append(LIST_SPLIT)
+    p = crypto.Crypto(key=key,
+               text=[C])
+    P = p.decrypt()
+    print "Result: Data Embed is: ", P    
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Dynamic Crypto Stegano Daemon")
+    parser.add_argument('-i','--image_embed',dest='image_embed',required=True,help='Put your image you want embed data into it',metavar='')
+    parser.add_argument('-o','--image_retriev',dest='image_retriev',required=True,help='Put your image you want embed data into it',metavar='')
+    parser.add_argument('-k','--key',dest='key',required=True,help='Key use to encrypt plaintext',metavar='')
+    parser.add_argument('-t','--plaintext',dest='plaintext',required=True,help='Plain text embed private into image',metavar='')
+    args = parser.parse_args()
+    ans=True
+    while ans:
+        print ("""
+        1.Embed plaintext in picture
+        2.Perform retriev plaintext in image
+        3.Exit/Quit
+        """)
+        number_select=input("What would you like to do?" )
+        if number_select==1: 
+            print("\nEmbed plaintext in picture")
+            start_embed(args.plaintext, args.key, args.image_embed, args.image_retriev)
+            ans=True 
+        elif number_select==2:
+            print "\n Perform retriev plaintext in image"
+            start_retriev(args.key, args.image_retriev)
+            ans=True  
+        elif number_select==3:
+            print("\n Goodbye")
+            ans=False
+        elif ans !="":
+            print("\n Not Valid Choice Try again")
 
 
 if __name__ == '__main__':
